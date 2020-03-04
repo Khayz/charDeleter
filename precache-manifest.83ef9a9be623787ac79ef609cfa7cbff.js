ed5ebc52119c36ec3236c0f8e220f838dadfe044
Khay <altaircarn@gmail.com>
2020-03-04 13:41:48 -0600

    Updates

---
+++ precache-manifest.83ef9a9be623787ac79ef609cfa7cbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4f03642dc2c927071394f082c0096f",
+    "url": "/charDeleter/index.html"
+  },
+  {
+    "revision": "778e4842bdf8e31b7f76",
+    "url": "/charDeleter/static/css/main.e2f73bf3.chunk.css"
+  },
+  {
+    "revision": "77a6540be4c6eb9cca4c",
+    "url": "/charDeleter/static/js/2.7f62c3a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/charDeleter/static/js/2.7f62c3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778e4842bdf8e31b7f76",
+    "url": "/charDeleter/static/js/main.cc006100.chunk.js"
+  },
+  {
+    "revision": "b6b9a5e59643b5d4ee03",
+    "url": "/charDeleter/static/js/runtime-main.81c0b248.js"
+  }
+]);
